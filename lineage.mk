1585bb475cf85f9ebe7a6662104665e5af36912d
Nasrellah Irhmar <nasreirma@gmail.com>
2018-07-04 12:31:02 +0100

    update names

--- lineage.mk
+++ lineage.mk
@@ -14,7 +14,7 @@ TARGET_SCREEN_WIDTH := 1080
 DEVICE_RESOLUTION := 1080x1920
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := Infinix NOTE 4 Pro
+PRODUCT_DEVICE := X571
 PRODUCT_NAME := lineage_X571
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix NOTE 4 Pro
